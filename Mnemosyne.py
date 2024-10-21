3573ecbb3ecff4c906c477a9199adc2fdffad2da
Code Monkey Cybersecurity <git@codemonkey.net.au>
2024-10-21 22:04:06 +0800

    Update Mnemosyne.py

--- Mnemosyne.py
+++ Mnemosyne.py
@@ -1,10 +1,27 @@
 from flask import Flask, request
 import os
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
 LOG_DIR = "/var/log/cybermonkey-received-logs"
+
+# Load configuration from ssl.json
+def load_ssl_config():
+    with open('ssl.json', 'r') as config_file:
+        return json.load(config_file)
+
+if __name__ == '__main__':
+    ssl_config = load_ssl_config()
+    ssl_cert = ssl_config.get("ssl_cert")
+    ssl_key = ssl_config.get("ssl_key")
+    
+    if ssl_cert and ssl_key:
+        # Enable IPv6 by binding to '::'
+        app.run(host='::', port=443, ssl_context=(ssl_cert, ssl_key))
+    else:
+        print("SSL certificate or key not found in ssl.json.")
 
 @app.route('/upload_logs', methods=['POST'])
 def upload_logs():
@@ -24,6 +41,3 @@ def upload_logs():
     logfile.save(save_path)
     
     return "Log uploaded successfully.", 200
-
-if __name__ == '__main__':
-    app.run(host='::', port=5000)
